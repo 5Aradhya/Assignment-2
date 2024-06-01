232a646aab567ad801dfa6b08e5f2442b5f061b3
Aradhya uttam <aradhyauttam15@gmail.com>
2024-06-01 23:36:40 +0530

    Add files via upload

---
+++ Que-9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+// Write a program to calculate volume of cuboid 
+int l,b,h;
+printf(" enter your length :");
+scanf("%d",&l);
+printf("enter your breadth :");
+scanf("%d",&b);
+printf("enter your height : ");
+scanf("%d",&h);
+printf(" volume of cuboid : %d ", l*b*h);
+
+}
