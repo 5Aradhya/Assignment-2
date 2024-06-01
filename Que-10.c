103361b2881495b78d4d9a5d4c1ae7fe4736cc6c
Aradhya uttam <aradhyauttam15@gmail.com>
2024-06-01 23:37:43 +0530

    Add files via upload

---
+++ Que-10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    // Write a program to input three character from the user and display characters with their ASCII codes 
+    char a ;
+    printf(" Enter a character : ");
+    scanf("%c",& a);
+printf(" convet char to ASCII %c=%d",a,a);
+
+}
