96a9ea69d5f720a38301b8351975cfcd819c1fca
Aradhya uttam <aradhyauttam15@gmail.com>
2024-06-01 23:36:13 +0530

    Add files via upload

---
+++ Que7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    // Write a program to calculate circumference of a circle 
+    int radius;
+    printf(" Enter your raduius : "); 
+    scanf("%d",&radius);
+    printf(" circumference of circle : %f", 2*3.14*radius );
+
+}
